76883f039d9aa5633ebd94323ef8eaa29db3babb
junayed-cft <hossain@vimond.com>
2024-07-05 22:40:10 +0600

    live-subtitle-generator: web socket app initialized

---
+++ websocket-server/src/main/java/com/playground/streaming/transcribeStreaming/TranscribeStreamingApp.java
@@ -0,0 +1,13 @@
+package com.playground.streaming.transcribeStreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranscribeStreamingApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranscribeStreamingApp.class, args);
+	}
+
+}
---
+++ websocket-server/src/test/java/com/playground/streaming/transcribeStreaming/TranscribeStreamingAppTests.java
@@ -0,0 +1,13 @@
+package com.playground.streaming.transcribeStreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranscribeStreamingAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
